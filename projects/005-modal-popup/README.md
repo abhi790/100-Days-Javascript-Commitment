33ec60371d30c6f37191995f373f7e700666f156
abhi790 <abhimanyu.roy4000@gmail.com>
2026-03-10 15:56:00 +0530

    project005 - Minimum Viable Product, first version of the Modal popup is ready. share to twitter is not implemented.

--- projects/005-modal-popup/README.md
+++ projects/005-modal-popup/README.md
@@ -13,6 +13,9 @@ Modal dialog component.
 ✅ Modal content
 ✅ Smooth animation
 ✅ Responsive design
+✅ Copy to clipboard fully functional
+✅ Used Random data in the modal, on click of openModal fresh data load randomly
+✅ actual share to twitter is not implemented only feedback is shown.
 
 ## 🛠️ Technologies Used
 
@@ -29,6 +32,7 @@ Modal dialog component.
 - 'click' Event listeners
 - Variable manipulation
 - DOM updates
+- Math.random used select random data for modal
 - Button click handling
 - Clean UI using CSS only
 - Simple Design
@@ -36,10 +40,20 @@ Modal dialog component.
 - CSS overlays
 - Modal patterns
 - Event handling
-- Class toggling
+- Flexbox centering
+- Overlay pattern
+- Event bubbling
+- stopPropagation()
+- DOM event listeners
+- setTimeOut used to remove feedback after 1500msec
+
+## 📚 Scope for Improvent
+
+- Share to twitter and other social media can be implemented.
+-
 
 ## 📸 Preview
 
 <!-- change this with original project image file after completion of the project -->
 
-## ![Preview Image](./005-modal-popup)
+## ![Preview Image](./005-modal-popup.png)
--- projects/005-modal-popup/script.js
+++ projects/005-modal-popup/script.js
@@ -1,1 +1,70 @@
-console.log("Script loading perfectly");
+const modalOverlay = document.querySelector(".modal-overlay");
+const modal = document.querySelector(".modal-content");
+const btnOpen = document.querySelector(".btn-open");
+const btnClose = document.querySelector(".cross");
+const btnCopy = document.querySelector(".btn-copy");
+const btnShare = document.querySelector(".btn-share");
+const heading = document.querySelector("#heading");
+const content = document.querySelector(".content");
+const btnContainer = document.querySelector(".btn-container");
+modalOverlay.addEventListener("click", closeModal);
+modal.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+btnOpen.addEventListener("click", openModal);
+
+// Implement shared to twitter
+function shareToTwitter() {
+  const shareFeedback = document.createElement("span");
+  shareFeedback.innerHTML = `<i class="fa-brands fa-twitter"></i> Shared`;
+  shareFeedback.classList.add("share-feedback");
+  btnContainer.appendChild(shareFeedback);
+  setTimeout(() => shareFeedback.remove(), 1500);
+}
+function renderData(randomData) {
+  heading.textContent = randomData.heading;
+  content.textContent = randomData.description;
+}
+function getRandomData() {
+  const randomIndex = Math.floor(Math.random() * data.length);
+  return data[randomIndex];
+}
+
+//   implement showCopied feature
+function showCopied() {
+  const feedback = document.createElement("span");
+  feedback.textContent = "✅ Copied";
+  feedback.classList.add("copy-feedback");
+  btnContainer.appendChild(feedback);
+
+  setTimeout(() => feedback.remove(), 1500);
+}
+
+//   open modal when btnOpen is clicked
+function openModal() {
+  modalOverlay.style.display = "flex";
+  renderData(getRandomData());
+}
+function closeModal() {
+  modalOverlay.style.display = "none";
+}
+function closeOnEscapeKey(e) {
+  if (e.key === "Escape") closeModal();
+}
+function copyToClipboard() {
+  navigator.clipboard.writeText(content.textContent);
+  showCopied();
+}
+
+//   close modal on button cross click in UI
+btnClose.addEventListener("click", closeModal);
+
+//   close modal on esc key pressed
+document.addEventListener("keydown", closeOnEscapeKey);
+
+//   copy to clipboard functionality implementation
+btnCopy.addEventListener("click", copyToClipboard);
+
+//   share functionality
+btnShare.addEventListener("click", shareToTwitter);
