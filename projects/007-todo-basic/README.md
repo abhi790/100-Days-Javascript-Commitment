5ed67b37bf56ac4ebc4d690113dee49393fe15c4
abhi790 <abhimanyu.roy4000@gmail.com>
2026-03-12 16:55:02 +0530

    007-todo basic - MVP is ready to push to production, thumbnail of the project updated, strike through implemented, checkbox designed, project readme updated, script and style are organized in seperate file

---
+++ projects/007-todo-basic/README.md
@@ -0,0 +1,41 @@
+## Project 007 - Todo List (Basic)
+
+# Todo List (Basic)
+
+Simple todo tracker.
+
+## Features
+
+✅ Add todos
+✅ Mark complete
+✅ Delete todos
+✅ Task counter
+✅ List display
+✅ Responsive design
+
+## 🛠️ Technologies Used
+
+- HTML5
+- CSS3 (Flexbox, Gradients)
+- Vanilla JavaScript (ES6+)
+
+## Live Demo
+
+🔗 [View Live](https://100-days-javascript-commitment.netlify.app/projects/007-todo-basic/)
+
+## What I Learned
+
+- Array manipulation
+- DOM rendering
+- Event delegation
+- CRUD operations
+- 'click' Event listeners
+- Variable manipulation
+- DOM updates
+- Button click handling
+- Clean UI using CSS only
+- Simple Design
+
+## 📸 Preview
+
+## ![Preview Image](./007-todo-basic.png)
--- projects/007-todo-basic/script.js
+++ projects/007-todo-basic/script.js
@@ -1,1 +1,106 @@
-console.log("Script loaded successfuly");
+const todoContainer = document.querySelector(".todo-container");
+const btnAddTask = document.querySelector("#add-btn");
+const input = document.querySelector("#input");
+const count = document.querySelector(".count");
+let todos = [
+  {
+    id: 1,
+    task: "Complete CSS checkbox tutorial",
+    completed: false,
+  },
+  {
+    id: 2,
+    task: "Build todo app with custom checkboxes",
+    completed: true,
+  },
+  {
+    id: 3,
+    task: "Practice JavaScript array methods",
+    completed: false,
+  },
+];
+renderTodos();
+//   update count
+function updateCount() {
+  count.textContent = `${todos.length}`;
+}
+//   display todos and call updateCount
+function renderTodos() {
+  todoContainer.innerHTML = "";
+  todos.forEach((todo) => {
+    const todoItem = document.createElement("li");
+    todoItem.className = "list-item";
+    todoItem.innerHTML = `<div class="item">
+                <input type="checkbox" ${todo.completed ? "checked" : ""} name="" value="" />
+                <p style="${todo.completed ? "text-decoration:line-through" : ""}">${todo.task}</p>
+              </div>
+              <span class="delete-btn"><i class="fa-solid fa-trash"></i></span>`;
+
+    todoContainer.insertAdjacentElement("beforeend", todoItem);
+
+    //   adding event listener on trash icon
+    const btn = todoItem.querySelector(".delete-btn");
+    btn.addEventListener("click", () => {
+      todoItem.remove();
+      deleteTodo(todo.id);
+    });
+
+    //checbox toggle event
+    const checkbox = todoItem.querySelector('input[type="checkbox"]');
+    checkbox.addEventListener("change", () => toggleComplete(todo));
+  });
+  updateCount();
+}
+
+//   toggle checkbox on change
+function toggleComplete(todo) {
+  if (todo) {
+    todo.completed = !todo.completed;
+    renderTodos();
+  }
+}
+
+function deleteTodo(id) {
+  // using filter to eliminate particular todo on which click occur
+  todos = todos.filter((todo) => todo.id !== id);
+  updateCount();
+}
+
+function createToDoObject(task) {
+  const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+  return { id, task, completed: false };
+}
+
+//logic to add new todos to todoContainer and update todos array
+function addToDos() {
+  const task = input.value.trim();
+  input.value = "";
+  if (!task) {
+    alert("Please enter a todo.");
+    return;
+  }
+  // update the todos array
+  const todo = createToDoObject(task);
+  todos.push(todo);
+  renderTodos();
+}
+
+//   logic to implement clear all
+function clearAllTodos() {
+  if (todos.length > 0) {
+    if (confirm("Are you sure want to clear all todos?")) {
+      todos = [];
+      renderTodos();
+    }
+  }
+}
+
+//   add new task on button click
+btnAddTask.addEventListener("click", addToDos);
+//   add new task on enter press
+document.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") addToDos();
+});
+
+//   clear all todos
+document.querySelector(".clear").addEventListener("click", clearAllTodos);
