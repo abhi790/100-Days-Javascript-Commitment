bd013f512f4d38edc0f9546e1a880fbe8083f10b
abhi790 <abhimanyu.roy4000@gmail.com>
2026-04-06 22:54:59 +0530

    chore(p022): link stylesheet and script files

--- projects/021-scroll-totopbutton/script.js
+++ projects/021-scroll-totopbutton/script.js
@@ -1,1 +1,77 @@
-// logic of the app
+const ScrollModel = {
+  // state
+  _isVisible: false,
+  _scrollY: 0,
+
+  // getter and setter methods
+  getButtonState() {
+    return this._isVisible;
+  },
+  setButtonState(state) {
+    this._isVisible = state;
+  },
+
+  getScrollY() {
+    return this._scrollY;
+  },
+  setScrollY(value) {
+    this._scrollY = value;
+  },
+};
+
+const ScrollView = {
+  elements: {
+    btn: null,
+  },
+
+  init: function () {
+    this.elements.btn = document.getElementById("scrollTopBtn");
+  },
+
+  viewButton: function (isVisible) {
+    if (isVisible) {
+      this.elements.btn.style.opacity = 1;
+      this.elements.btn.style.visibility = "visible";
+    } else {
+      this.elements.btn.style.opacity = 0;
+      this.elements.btn.style.visibility = "hidden";
+    }
+  },
+
+  updateButton: function (isVisible) {
+    this.viewButton(isVisible);
+  },
+};
+
+const ScrollController = {
+  init: function () {
+    ScrollView.init();
+    ScrollView.updateButton(ScrollModel.getButtonState());
+    this.attachEventListeners();
+  },
+
+  handlers: {
+    handleButtonClick: function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    },
+    handleScroll: function (scrollY) {
+      ScrollModel.setScrollY(scrollY);
+      ScrollModel.setButtonState(scrollY >= 300);
+      ScrollView.updateButton(ScrollModel.getButtonState());
+    },
+  },
+
+  // attach listeners
+  attachEventListeners: function () {
+    ScrollView.elements.btn.addEventListener("click", () => {
+      this.handlers.handleButtonClick();
+    });
+    window.addEventListener("scroll", () => {
+      this.handlers.handleScroll(window.scrollY);
+    });
+  },
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  ScrollController.init();
+});
