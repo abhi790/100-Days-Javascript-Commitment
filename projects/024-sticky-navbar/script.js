2811420ee9c14c961ac3a4e1f8c01ddf3e1eac50
abhi790 <abhimanyu.roy4000@gmail.com>
2026-04-09 23:05:48 +0530

    feat(p024): implement sticky navbar with scroll detection and animal cards

--- projects/024-sticky-navbar/script.js
+++ projects/024-sticky-navbar/script.js
@@ -1,1 +1,14 @@
-// logic of the app
+const navbarEle = document.querySelector(".navbar");
+const topBarEle = document.querySelector(".top-bar");
+const threshold = topBarEle.offsetHeight;
+const navbarHeight = navbarEle.offsetHeight;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= threshold) {
+    navbarEle.classList.add("sticky");
+    document.body.style.paddingTop = navbarHeight + "px";
+  } else {
+    navbarEle.classList.remove("sticky");
+    document.body.style.paddingTop = "0px";
+  }
+});
